67006539e4a7fee1965684f6b0ba4973761d23a0
Dan Johnson <dan@designmynight.com>
2018-01-31 19:41:36 +0000

    feat: created abstract aggregation class

---
+++ src/DesignMyNight/Elasticsearch/Aggregations/Aggregation.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace DesignMyNight\Elasticsearch\Aggregations;
+
+abstract class Aggregation implements AggregationContract
+{
+    /**
+     * The name of the aggregation
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * The field you wish to aggregate data from
+     * @var string
+     */
+    protected $field;
+
+    /**
+     * Get the name of the aggregation
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the field name to aggregate data from
+     * @return string
+     */
+    public function getField(): string
+    {
+        return $this->field;
+    }
+
+    /**
+     * Set the name of the aggregation
+     * @param  string $name
+     * @return self
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Set the field to aggregate data from
+     * @param  string $field
+     * @return self
+     */
+    public function setField(string $field): self
+    {
+        $this->field = $field;
+
+        return $this;
+    }
+
+    /**
+     * Returns an array of the aggregation which
+     * can be interpreted by the query builder
+     * @return array
+     */
+    public function getAggregation(): array
+    {
+        return [
+            $this->getName(),
+            $this->getType(),
+            $this->getOptions()
+        ];
+    }
+}
